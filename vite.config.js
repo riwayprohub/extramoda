a4e6c24b4cf1d89651831d0633e80de835b3cf51
riwayprohub <riway.pro@gmail.com>
2026-09-08 02:27:24 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
+import { fileURLToPath, URL } from "node:url"
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/riwayprohub/extramoda/',
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    port: 5173,
+    proxy: {
+      "/uploads": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+})
