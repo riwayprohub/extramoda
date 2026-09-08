5c5a81bcc7072648d140c4bef785035f88c74a3d
riwayprohub <riway.pro@gmail.com>
2026-09-08 02:26:19 -0400

    Delete assets directory

--- assets/hero-CnMZ0dP8.js
+++
@@ -1,1 +0,0 @@
-import{C as o}from"./index-DOTtjKdr.js";const s=()=>o.get("/hero"),r=e=>o.post("/hero",e),a=e=>o.delete(`/hero/${e}`);export{a as d,s as g,r as u};
--- assets/orders-DRrMKOdS.js
+++
@@ -1,1 +0,0 @@
-import{C as r}from"./index-DOTtjKdr.js";const s=e=>r.post("/orders",e),o=()=>r.get("/orders/mine");export{s as c,o as g};
